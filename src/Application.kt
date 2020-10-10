ae23517afac57465e830caca6d9f56df4871331d
unknown <espen.hoines@gmail.com>
2020-10-10 10:30:04 +0200

    initial commit

---
+++ src/Application.kt
@@ -0,0 +1,85 @@
+package com.Ktor.Testing
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(DefaultHeaders)
+    val client = HttpClient(Apache) {}
+
+    routing {
+        get("/") {
+            call.respondText("Testing Ktor! Have a nice day", contentType = ContentType.Text.Plain)
+        }
+
+        get("/hello") {
+            call.respondHtml {
+                head {
+                    title { +"Ktor testing" }
+
+
+                }
+
+                body {
+                    h1 { +"Hello World" }
+                    h2 { +"Random image from Unsplashed to improve your day!" }
+                    img { src = "https://source.unsplash.com/random" }
+                }
+            }
+        }
+
+        get("/html-dsl") {
+            call.respondHtml {
+                body {
+                    h1 { +"HTML" }
+                    ul {
+                        for (n in 1..10) {
+                            li { +"$n" }
+                        }
+                    }
+                }
+            }
+        }
+
+        get("/styles.css") {
+            call.respondCss {
+                body {
+                    backgroundColor = Color.red
+                }
+                p {
+                    fontSize = 2.em
+                }
+                rule("p.myclass") {
+                    color = Color.blue
+                }
+            }
+        }
+    }
+}
+
+fun FlowOrMetaDataContent.styleCss(builder: CSSBuilder.() -> Unit) {
+    style(type = ContentType.Text.CSS.toString()) {
+        +CSSBuilder().apply(builder).toString()
+    }
+}
+
+fun CommonAttributeGroupFacade.style(builder: CSSBuilder.() -> Unit) {
+    this.style = CSSBuilder().apply(builder).toString().trim()
+}
+
+suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
+    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+}
